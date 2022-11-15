e0b40b50aeacf7937720812c74a4dc257ae533de
siebe <r0804405@student.thomasmore.be>
2022-11-15 21:29:58 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,27 +5,41 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class Rider(BaseModel):
+    id: int
     naam: str
     leeftijd: int
     land: str
     ploeg: str
-    geboortedatum: str
-    gewicht: int
-    lengte: int
     punten: int
-    
+
+def Sorteer(lijst):
+    for i in lijst:
+        print(i)
+    return lijst
+
+
 riders = []
 
-riders.append(Rider(naam="Tadej Pogačar", leeftijd=24, land="Slovenië", ploeg="UAE Team Emirates", geboortedatum="21/09/1998", gewicht=66, lengte=176, punten=4839))
-riders.append(Rider(naam="Wout van Aert", leeftijd=28, land="België", ploeg="Jumbo-Visma", geboortedatum="15/09/1994", gewicht=78, lengte=190, punten=3722))
-riders.append(Rider(naam="Remco Evenepoel", leeftijd=22, land="België", ploeg="Soudal-Quickstep", geboortedatum="25/01/2000", gewicht=63, lengte=171, punten=3602))
-riders.append(Rider(naam="Jonas Vingegaard", leeftijd=25, land="Denemarken", ploeg="Jumbo-Visma", geboortedatum="10-12-1996", gewicht=60, lengte=175,punten=3317))
-riders.append(Rider(naam="Aleksandr Vlasov", leeftijd=26, land="Rusland", ploeg="BORA-Hansgrohe", geboortedatum="23-04-1996", gewicht=68, lengte=186,punten=2105))
+riders.append(Rider(id=1, naam="Tadej Pogačar", leeftijd=24, land="Slovenië", ploeg="UAE Team Emirates", punten=4839))
+riders.append(Rider(id=2, naam="Wout van Aert", leeftijd=28, land="België", ploeg="Jumbo-Visma", punten=3722))
+riders.append(Rider(id=3, naam="Remco Evenepoel", leeftijd=22, land="België", ploeg="Soudal-Quickstep", punten=3602))
+riders.append(Rider(id=4, naam="Jonas Vingegaard", leeftijd=25, land="Denemarken", ploeg="Jumbo-Visma", punten=3317))
+riders.append(Rider(id=5, naam="Aleksandr Vlasov", leeftijd=26, land="Rusland", ploeg="BORA-Hansgrohe", punten=2105))
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"This is the home page"}
+
+@app.get("/leaderboard")
+async def get_leaderboard():
+    topbord = Sorteer(riders)
+    return {topbord}
 
 @app.get("/riders")
 async def get_riders():
+    return {"riders": riders}
+
+@app.post("/addrider/")
+async def add_rider(riderid: int, ridernaam: str, riderleeftijd: int, riderland: str, riderploeg: str, riderpunten: int):
+    riders.append(Rider(id=riderid, naam=ridernaam, leeftijd=riderleeftijd, land=riderland, ploeg=riderploeg, punten=riderpunten))
     return {"riders": riders}

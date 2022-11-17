4be0cfb76a5ad8fb07224622dbfebf69825bfb9d
siebe <r0804405@student.thomasmore.be>
2022-11-17 23:19:27 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,12 +5,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ['*']
+origins = ['https://siebevr.github.io/, http://localhost:8000']
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST, PUT, DELETE"],
     allow_headers=["*"],
 )
 
